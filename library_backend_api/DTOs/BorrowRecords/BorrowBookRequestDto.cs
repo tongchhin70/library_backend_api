b0eefbedf9cbf5dfbabbfbd5a11f27ed2e4269c7
tongchhin70 <tongchhin70@gmail.com>
2026-04-26 13:21:32 -0700

    added error message for borrow request

--- library_backend_api/DTOs/BorrowRecords/BorrowBookRequestDto.cs
+++ library_backend_api/DTOs/BorrowRecords/BorrowBookRequestDto.cs
@@ -5,9 +5,9 @@ namespace library_backend_api.DTOs.BorrowRecords;
 // Incoming payload used to borrow a book for a member.
 public class BorrowBookRequestDto
 {
-    [Range(1, int.MaxValue)]
+    [Range(1, int.MaxValue, ErrorMessage = "BookId must be greater than 0.")]
     public int BookId { get; set; }
 
-    [Range(1, int.MaxValue)]
+    [Range(1, int.MaxValue, ErrorMessage = "MemberId must be greater than 0.")]
     public int MemberId { get; set; }
 }
